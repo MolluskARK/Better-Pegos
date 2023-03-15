b0336b992b4634f9b93c39b7f0a8ec15614c945c
MolluskARK <molluskark@gmail.com>
2023-03-15 14:43:46 -0600

    Add code

--- src/BetterPegos.cpp
+++ src/BetterPegos.cpp
@@ -1,39 +1,111 @@
 #include "API/ARK/Ark.h"
 
-// Called when the server is ready
-void Plugin_ServerReadyInit()
+struct FOutParmRec
 {
-    // Do post-"server ready" initialization here
+    UProperty*& PropertyField() { return *GetNativePointerField<UProperty**>(this, "FOutParmRec.Property"); }
+    uint8*& PropAddrField() { return *GetNativePointerField<uint8**>(this, "FOutParmRec.PropAddr"); }
+    FOutParmRec*& NextOutParmField() { return *GetNativePointerField<FOutParmRec**>(this, "FOutParmRec.NextOutParm"); }
+};
+
+struct FFrame
+{
+    UFunction*& NodeField() { return *GetNativePointerField<UFunction**>(this, "FFrame.Node"); }
+    uint8*& LocalsField() { return *GetNativePointerField<uint8**>(this, "FFrame.Locals"); }
+    FOutParmRec*& OutParmsField() { return *GetNativePointerField<FOutParmRec**>(this, "FFrame.OutParms"); }
+};
+
+UClass* CryoClass;
+UFunction* PegoCanStealFunc;
+UProperty* ItemProp;
+UProperty* CanStealProp;
+
+DECLARE_HOOK(UObject_ProcessInternal, void, UObject*, FFrame*, void* const);
+void Hook_UObject_ProcessInternal(UObject* _this, FFrame* Stack, void* const Result)
+{
+    UObject_ProcessInternal_original(_this, Stack, Result);
+
+    if (Stack->NodeField() != PegoCanStealFunc)
+        return;
+
+    UPrimalItem* item = *(UPrimalItem**)(Stack->LocalsField() + ItemProp->Offset_InternalField());
+    if (!item->IsA(CryoClass))
+        return;
+
+    bool* canSteal = nullptr;
+    for (FOutParmRec* rec = Stack->OutParmsField(); rec; rec = rec->NextOutParmField()) {
+        if (rec->PropertyField() == CanStealProp) {
+            canSteal = (bool*)rec->PropAddrField();
+            break;
+        }
+    }
+    if (canSteal)
+        *canSteal = false;
 }
 
-// ArkServerApi hook that triggers once when the server is ready
+// Called when the server is ready
+void OnServerReady()
+{
+    CryoClass = Globals::FindClass("BlueprintGeneratedClass /Game/Extinction/CoreBlueprints/Weapons/PrimalItem_WeaponEmptyCryopod.PrimalItem_WeaponEmptyCryopod_C");
+    if (!CryoClass) {
+        Log::GetLog()->error("UClass not found: PrimalItem_WeaponEmptyCryopod_C");
+        return;
+    }
+
+    UClass* pegoClass = Globals::FindClass("BlueprintGeneratedClass /Game/PrimalEarth/Dinos/Pegomastax/Pegomastax_Character_BP.Pegomastax_Character_BP_C");
+    if (!pegoClass) {
+        Log::GetLog()->error("UClass not found: Pegomastax_Character_BP_C");
+        return;
+    }
+
+    PegoCanStealFunc = pegoClass->FindFunctionByName(FName("CanStealItem", EFindName::FNAME_Find), EIncludeSuperFlag::ExcludeSuper);
+    if (!PegoCanStealFunc) {
+        Log::GetLog()->error("UFunction not found: Pegomastax_Character_BP_C::CanStealItem()");
+        return;
+    }
+
+    for (UProperty* prop = PegoCanStealFunc->PropertyLinkField(); prop; prop = prop->PropertyLinkNextField()) {
+        if (prop->NameField().ToString().Compare("Item") == 0)
+            ItemProp = prop;
+        else if (prop->NameField().ToString().Compare("canSteal") == 0)
+            CanStealProp = prop;
+    }
+    if (!ItemProp)
+        Log::GetLog()->error("UProperty not found: Item");
+    if (!CanStealProp)
+        Log::GetLog()->error("UProperty not found: canSteal");
+    if (!ItemProp || !CanStealProp)
+        PegoCanStealFunc = nullptr;
+}
+
+// Hook that triggers once when the server is ready
 DECLARE_HOOK(AShooterGameMode_BeginPlay, void, AShooterGameMode*);
 void Hook_AShooterGameMode_BeginPlay(AShooterGameMode* _this)
 {
     Log::GetLog()->info("Hook_AShooterGameMode_BeginPlay()");
     AShooterGameMode_BeginPlay_original(_this);
 
-    // Call Plugin_ServerReadyInit() for post-"server ready" initialization
-    Plugin_ServerReadyInit();
+    // Call OnServerReady() for post-"server ready" initialization
+    OnServerReady();
 }
 
 // Called by ArkServerApi when the plugin is loaded
 extern "C" __declspec(dllexport) void Plugin_Init()
 {
-    // Do pre-"server ready" initialization here
     Log::Get().Init(PROJECT_NAME);
 
     ArkApi::GetHooks().SetHook("AShooterGameMode.BeginPlay", Hook_AShooterGameMode_BeginPlay,
         &AShooterGameMode_BeginPlay_original);
+    ArkApi::GetHooks().SetHook("UObject.ProcessInternal", Hook_UObject_ProcessInternal,
+        &UObject_ProcessInternal_original);
 
-    // If the server is ready, call Plugin_ServerReadyInit() for post-"server ready" initialization
+    // If the server is ready, call OnServerReady() for post-"server ready" initialization
     if (ArkApi::GetApiUtils().GetStatus() == ArkApi::ServerStatus::Ready)
-        Plugin_ServerReadyInit();
+        OnServerReady();
 }
 
 // Called by ArkServerApi when the plugin is unloaded
 extern "C" __declspec(dllexport) void Plugin_Unload()
 {
-    // Do cleanup here
     ArkApi::GetHooks().DisableHook("AShooterGameMode.BeginPlay", Hook_AShooterGameMode_BeginPlay);
+    ArkApi::GetHooks().DisableHook("UObject.ProcessInternal", Hook_UObject_ProcessInternal);
 }
